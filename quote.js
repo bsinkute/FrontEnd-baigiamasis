3b64a01989e4931a4bca97087a297cd36931d850
Bronislava <sinkute.bronislava@gmail.com>
2024-07-04 21:14:53 +0300

    added promise syntax and extract updateQuote function

--- quote.js
+++ quote.js
@@ -1,15 +1,28 @@
 function getNewQuote() {
-    fetch('https://api.quotable.io/random')
-        .then(response => response.json())
-        .then(data => {
-            const quoteText = data.content;
-            const quoteAuthor = data.author;
-            const quoteElement = document.getElementById('quote');
-            const authorElement = document.getElementById('quote-author');
-            quoteElement.textContent = `"${quoteText}"`;
-            authorElement.textContent = `- ${quoteAuthor}`;
+    return fetch('https://api.quotable.io/random') //pats fetchas grazina promise todel nereikia dar daryti, kad promisas grazintu promisa
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.json();
         })
-        .catch(error => console.error('Error fetching quote:', error));
+        .then(data => updateQuote(data))
+        .catch(error => {
+            console.error('Error fetching quote:', error);
+        });
+}
+
+function updateQuote(data) {
+    if (data) {
+        const quoteText = data.content;
+        const quoteAuthor = data.author;
+        const quoteElement = document.getElementById('quote');
+        const authorElement = document.getElementById('quote-author');
+        quoteElement.textContent = `"${quoteText}"`;
+        authorElement.textContent = `- ${quoteAuthor}`;
+    } else {
+        console.error('No data to update');
+    }
 }
 
 getNewQuote();
