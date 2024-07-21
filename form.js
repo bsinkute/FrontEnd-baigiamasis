ef96ef075d35d7429262b6dc221e7fe55c974f24
Bronislava <sinkute.bronislava@gmail.com>
2024-07-21 18:06:42 +0300

    Fix response parsing

--- form.js
+++ form.js
@@ -173,13 +173,10 @@ document.addEventListener("DOMContentLoaded", function() {
             },
             body: JSON.stringify({ id: 0, date: date, events: events })
         })
-        .then(response => {
+        .then((response) => {
             if (!response.ok) {
                 throw new Error('Network response was not ok');
             }
-            return response.json();
-        })
-        .then(() => {
             return fetchNotes();
         })
         .catch(error => {
